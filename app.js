46ebd8c644c489aa7c216a290edb8795c7a47d64
Kathryn Xing <kxxing@edu.uwaterloo.ca>
2020-09-14 23:07:20 -0400

    install pagckages

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+app.set("view engine", "ejs");
+app.get('/',function(req, res){
+    res.render("home");
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Server Has Started!");
+  });
